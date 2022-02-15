f79d95dd5d23d65f834fcd793b1bdb739d760c5d
Camiloatalaya <developeratalaya2@outlook.com>
2022-02-15 17:20:17 -0500

    feat: agregando el componente de List que se encarga de la parte de mostrar los datos que el usuario agrego con su funcionalidad de eliminar y editar los datos

---
+++ src/Components/List/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+interface Props {
+	arrayInfo: any[]
+	deleteElemet: Function
+	setDataEdit: Function
+}
+
+export const List = ({ arrayInfo, deleteElemet, setDataEdit }: Props) => {
+	return (
+		<div>
+			<h2>Lista de datos Agregados</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Pais</th>
+						<th>Medallas</th>
+						<th>Jugadores</th>
+						<th>Eliminar</th>
+						<th>Actualizar</th>
+					</tr>
+				</thead>
+				<tbody>
+					{arrayInfo.map((data, i) => (
+						<tr key={i}>
+							<td>{data.country}</td>
+							<td>{data.medals}</td>
+							<td>{data.players}</td>
+							<td>
+								<button onClick={() => deleteElemet(data.id)}>Eliminar</button>
+							</td>
+							<td>
+								<button
+									onClick={() => {
+										setDataEdit(data)
+									}}
+								>
+									Actualizar
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
+}
